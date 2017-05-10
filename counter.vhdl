68e428635ea2e5e011f0f1515fbad2f76eedef9f
Ahmad Fatoum <ahmad@a3f.at>
2017-05-10 22:38:31 +0200

    Using BUFFER instead of temp signal

--- counter.vhdl
+++ counter.vhdl
@@ -7,24 +7,22 @@ use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
 entity counter_e is
-    port (clk : in std_logic; pulse : out std_logic);
+    port (clk : in std_logic; pulse : buffer std_logic);
 end entity counter_e;
 
         
 architecture behav of counter_e is
     signal counter: integer range 0 to 5;
-    signal trigger: std_logic := '1';
 begin process(clk)
 begin
-    if clk'event and clk='1' then
+    --if clk'event and clk='1' then
+    if rising_edge(clk) then
         if counter < 5 then
             counter <= counter + 1;
+            pulse <= '0'; -- can't we set a default value easily?
         else
-            pulse <= trigger;
-            trigger <= not trigger;
+            pulse <= not pulse;
         end if;
-    else
-        pulse <= '0';
     end if;
 end process;
 end architecture behav;
